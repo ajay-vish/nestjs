8387945d2bbf13a1bc3280e1999186e76fde8fc8
Ajay Vishwakarma <ajay.vishwakarma@sokrati.com>
2021-08-11 01:34:40 +0530

    Added category and product routes

---
+++ src/category/categories.module.ts
@@ -0,0 +1,15 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+
+import { CategoriesController } from './categories.controller';
+import { CategoriesService } from './categories.service';
+import { Categorieschema } from './categories.model';
+
+@Module({
+  imports: [
+    MongooseModule.forFeature([{ name: 'Category', schema: Categorieschema }]),
+  ],
+  controllers: [CategoriesController],
+  providers: [CategoriesService],
+})
+export class CategoriesModule {}
