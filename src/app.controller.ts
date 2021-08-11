7896b1685f6c29cbddf75044b1488dc472d84613
Ajay Vishwakarma <ajay.vishwakarma@sokrati.com>
2021-08-11 15:02:19 +0530

    Adding view

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Header } from '@nestjs/common';
+import { Controller, Get, Render } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -6,8 +6,8 @@ export class AppController {
 	constructor(private readonly appService: AppService) {}
 
 	@Get()
-	@Header('Content-Type', 'text/html')
-	getHello(): { name: string; professtion: string } {
-		return { name: 'Ajay Vishwakarma', professtion: 'Software developer' };
+	@Render('index')
+	root() {
+		return { name: 'Ajay Vishwakarma', profession: 'Software Engineer' };
 	}
 }
